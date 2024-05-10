3e1501bd76f5fb3c2097ff07cad067c7dea76e8d
dianemorales <dianemorales330@gmail.com>
2024-05-10 11:18:59 +0800

    Updates

--- assets/md.transition-legacy-B1KAFhmt.js
+++ assets/md.transition-legacy-B1KAFhmt.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BktLypF6.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-D3uY2uD2.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
